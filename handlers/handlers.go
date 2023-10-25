64f30cd22e13ae2ab6be941d750fd52a7e47be8b
huy <496334@student.fontys.nl>
2023-10-25 06:18:29 +0200

    Added username validator

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"webApp/db"
+	"webApp/pkg"
 	"webApp/render"
 )
 
@@ -32,18 +32,29 @@ func SigninPage(w http.ResponseWriter, r *http.Request) {
 	parsedTemplate := render.RenderTemplates(w, "signInPage.html")
 
 	if r.Method == http.MethodPost {
-
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-		email := r.FormValue("email")
-
-		db.AddUser(username, password, email)
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Failed to parse form", http.StatusBadRequest)
+			return
+		}
 	}
 
-	err := parsedTemplate.Execute(w, nil)
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	email := r.FormValue("email")
+
+	err := pkg.UsernameValidator(username, password, email)
+	if err != nil {
+		http.Error(w, "Couldn't validate user", http.StatusBadRequest)
+	}
+
+	http.Redirect(w, r, "/login", http.StatusFound)
+
+	err = parsedTemplate.Execute(w, nil)
 	if err != nil {
 		log.Fatal("Error executing template:", err)
 	}
+
 }
 
 func LoginPage(w http.ResponseWriter, r *http.Request) {
